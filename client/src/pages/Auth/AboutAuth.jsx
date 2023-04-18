0d46f0443981ddf073610ab9573b0875134a65c7
pallak02 <nikkipalak2003@gmail.com>
2023-04-18 17:03:17 +0530

    ccl

---
+++ client/src/pages/Auth/AboutAuth.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+const AboutAuth = () =>{
+    return(
+        <div className={"auth-container-1"}>
+            <h1>Signup to our News Website</h1>
+            <p>Get to know whats happening around the world</p>
+            <p>In-depth analysis of the day's top stories</p>
+            <p>Breaking news as it happens</p>
+            <p>Staying ahead of the curve with cutting-edge reporting</p>
+            <p style={{fontSize:"13px",color:"#666767"}}>A closer look at science and technology news </p>
+            <p style={{fontSize:"13px",color:"#007ac6"}}> Your source for local news and events </p>
+        </div>
+    )
+}
+export default AboutAuth
